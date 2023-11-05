6c570800ce64cdc1ac75b07ed5945fc46cad3f94
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-05 09:40:11 +0100

    database setup and connected

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+dotenv.config()
+
+const URI = process.env.MONGO_URI as string
 
 const app = express();
 
+mongoose.connect(URI).then(() => {
+  console.log('database is connected')
+}).catch((err) => {
+  console.log(err)
+})
+
 app.listen(3000, () => {
-  console.log("Hello here am i");
+  console.log("Hello here am i!");
 });
