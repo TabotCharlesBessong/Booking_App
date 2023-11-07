61a6800033179f13fdca087cc9ee6354622d7916
QUIN04 <tquinuel@gmail.com>
2023-11-07 08:14:58 +0100

    Navbar template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Header } from './components'
 
 const App = () => {
   return (
-    <div className='bg-red-500 w-full flex flex-1 items-center justify-center py-8' >
-    <h1 className='text-4xl text-center text-white capitalize' >lorem20</h1>
-    </div>
+    <>
+      <Header/>
+    </>
   )
 }
 
