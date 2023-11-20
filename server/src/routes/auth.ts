231e3c61ea1a5696bf145d308f5cbc16f1861c4a
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-20 17:16:46 +0100

    fixing usertype

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import User from '../models/users'
+import { CreateUser } from "src/@types/user";
 
 const router = Router()
 
-router.post('/create',(req,res) => {
+router.post('/create',(req:CreateUser,res) => {
   const {email,name,password} = req.body
   const user = new User({email,name,password})
   user.save()
