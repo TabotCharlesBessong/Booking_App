85add92302807db2180d093561d5e6fd8a6a1536
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-20 17:39:44 +0100

    adding an extra error

--- server/src/middleware/validator.ts
+++ server/src/middleware/validator.ts
@@ -20,6 +20,7 @@ export const validate = (schema:any): RequestHandler => {
     } catch (error) {
       if(error instanceof yup.ValidationError) {
         res.json({error:error.message})
+        res.status(422).json({error:error.message})
       }
     }
   }
