48b8f27bf4328e6910c3ae921985c1966d449e91
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-21 05:18:28 +0100

    sending mails

--- server/src/controller/user.ts
+++ server/src/controller/user.ts
@@ -1,10 +1,27 @@
 import { RequestHandler } from "express";
 import { CreateUser } from "../@types/user";
 import users from "../models/users";
+import nodemailer from "nodemailer"
+import { MAILTRAP_PASS, MAILTRAP_USER } from "../utils/variables";
 
 export const create:RequestHandler = async (req:CreateUser,res) => {
   const {email,name,password} = req.body
   const user = new users({email,name,password})
+
+  const transport = nodemailer.createTransport({
+    host:'sandbox.smtp.mailtrap.io',
+    port:2525,
+    auth:{
+      user:MAILTRAP_USER,
+      pass:MAILTRAP_PASS
+    }
+  })
+  transport.sendMail({
+    to:user.email,
+    from:"ebezebeatrice@gmail.com",
+    html:"<h1>12345678</h1>"
+  })
+  res.status(201).json({user})
   user.save()
   res.json({user})
 }
---
+++ server/src/utils/variables.ts
@@ -0,0 +1,6 @@
+
+const {env} = process as {env:{[key:string]:string}}
+
+export const MONGO_URI = env.MONGO_URI
+export const MAILTRAP_USER = env.MAILTRAP_USER
+export const MAILTRAP_PASS = env.MAILTRAP_PASS
