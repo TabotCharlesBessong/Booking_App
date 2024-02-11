d413b78f0b5f6f3614dda8bec0401588bc19920d
QUIN04 <tquinuel@gmail.com>
2024-02-11 14:46:49 +0100

    auth + movie dashborad

--- client/src/components/shared/header/Header.tsx
+++ client/src/components/shared/header/Header.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-      <nav className="p-4 text-white bg-black" h-1000
-      >
+    <div className="sticky top-0 z-10">
+      <nav className="p-4 text-white bg-black">
         <ul className="flex space-x-32">
-         
           <li className="mr-4">
             <a href="#" className="px-8 py-2 rounded hover:bg-gray-800">HOME</a>
           </li>
-          
           <li className="mr-4">
             <a href="#" className="px-4 py-2 rounded hover:bg-gray-800">MOVIES</a>
           </li>
-         
           <li className="mr-4">
             <a href="#" className="px-4 py-2 rounded hover:bg-gray-800">TICKETS STORE</a>
           </li>
-          
           <li className="mr-4">
             <a href="#" className="px-4 py-2 rounded hover:bg-gray-800">PROFILE</a>
           </li>
