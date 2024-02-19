a60320c23b738dd580975f863bba1ec1efff92db
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-02-19 19:36:28 +0100

    creating admin

--- server/src/routers/admin.router.ts
+++ server/src/routers/admin.router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { adminRegistration } from "./../controller/admin.controller";
 
 const router = Router()
 
-router.post('/signup') // admin signup
+router.post('/register',adminRegistration) // admin signup
 router.post('/login') // admin login
 router.get('/') // fetch all admins
 router.get('/:id') // get all admins by id
