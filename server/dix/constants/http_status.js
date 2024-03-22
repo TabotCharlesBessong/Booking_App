6758e5f868165042b9e22d85aafcd17bf3560284
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2024-03-22 14:55:39 +0100

    Delete server/dix directory

--- server/dix/constants/http_status.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HTTP_UNPROCESSABLE = exports.HTTP_SERVER_ERROR = exports.HTTP_NOT_FOUND = exports.HTTP_UNAUTHORIZED = exports.HTTP_BAD_REQUEST = void 0;
-exports.HTTP_BAD_REQUEST = 400;
-exports.HTTP_UNAUTHORIZED = 401;
-exports.HTTP_NOT_FOUND = 404;
-exports.HTTP_SERVER_ERROR = 500;
-exports.HTTP_UNPROCESSABLE = 422;
--- server/dix/routers/movie.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const movie_controller_1 = require("./../controller/movie.controller");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post("/", movie_controller_1.addMovie);
-router.get("/", movie_controller_1.getAllMovies);
-router.get("/:id", movie_controller_1.getMovieById);
-exports.default = router;
